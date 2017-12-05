6999f79b030197a3debe3fe5c2f09127afa75821
Max Wihlborg <max.wihlborg@ispy.se>
2017-12-05 20:47:46 +0100

    Refactor Day 1 to be consistent

--- day-01/index.js
+++ day-01/index.js
@@ -1,13 +1,29 @@
-export default (indata, offset = 1) => {
-  const arr = String(indata)
+const toArr = data =>
+  String(data)
     .trim()
     .split("")
     .map(n => ~~n);
 
-  const finalOffset = typeof offset === 'number' ? offset : offset(arr)
+export const one = data => {
+  const arr = toArr(data);
+
+  const offset = 1;
 
   const sum = arr.reduce(
-    (acc, n, i) => acc + (n === arr[(i + finalOffset) % arr.length] ? n : 0),
+    (acc, n, i) => acc + (n === arr[(i + offset) % arr.length] ? n : 0),
+    0
+  );
+
+  return sum;
+};
+
+export const two = data => {
+  const arr = toArr(data);
+
+  const offset = (arr.length / 2) | 2;
+
+  const sum = arr.reduce(
+    (acc, n, i) => acc + (n === arr[(i + offset) % arr.length] ? n : 0),
     0
   );
 
--- day-01/test.spec.js
+++ day-01/test.spec.js
@@ -1,21 +1,18 @@
 import { assert } from "chai";
-import run from "./index.js";
-
-const half = a => (a.length / 2) | 0;
+import { one, two } from "./index.js";
 
 describe("day 1", () => {
   describe("part 1", () => {
-    it("test 1", () => assert(run(1122) === 3));
-    it("test 2", () => assert(run(1111) === 4));
-    it("test 3", () => assert(run(1234) === 0));
-    it("test 4", () => assert(run(91212129) === 9));
+    it("test 1", () => assert.equal(one(1122), 3));
+    it("test 2", () => assert.equal(one(1111), 4));
+    it("test 3", () => assert.equal(one(1234), 0));
+    it("test 4", () => assert.equal(one(91212129), 9));
   });
-
   describe("part 2", () => {
-    it("test 1", () => assert(run(1212, half) === 6));
-    it("test 2", () => assert(run(1221, half) === 0));
-    it("test 3", () => assert(run(123425, half) === 4));
-    it("test 4", () => assert(run(123123, half) === 12));
-    it("test 5", () => assert(run(12131415, half) === 4));
+    it("test 1", () => assert.equal(two(1212), 6));
+    it("test 2", () => assert.equal(two(1221), 0));
+    it("test 3", () => assert.equal(two(123425), 4));
+    it("test 4", () => assert.equal(two(123123), 12));
+    it("test 5", () => assert.equal(two(12131415), 4));
   });
 });
